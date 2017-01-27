940060106cfb7ef1f6fd87667b8366bba40249ec
Nick Pesce <npesce@terpmail.umd.edu>
2017-01-27 05:12:23 +0000

    Added gamma correction for colors

--- controls.py
+++ controls.py
@@ -1,6 +1,24 @@
 from neopixel import *
 import colorsys
 
+GAMMA = [
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+    10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+    17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+    25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+    37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+    51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+    69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+    90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+    115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+    144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+    177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+    215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 ];
+
 # LED strip configuration:
 LED_COUNT	= 120     # Total number of LED pixels.
 LED_PIN		= 18      # GPIO pin connected to the pixels (must support PWM!).
@@ -30,6 +48,7 @@ class Led_Controller:
         self.inactive_ranges = list(ranges)
         self.num_leds = 0
         self.total_leds = sum(len(r) for r in self.ranges.itervalues())
+        self.pixels = [0]*self.total_leds
 
     def set_ranges(self, new_ranges):
         self.active_ranges = new_ranges
@@ -78,19 +97,18 @@ class Led_Controller:
         """Set a single pixel to a colour using HSV"""
         if index is not None:
             r, g, b = [int(n*255) for n in colorsys.hsv_to_rgb(h, s, v)]
-            ws2812.setPixelColorRGB(index, r, g, b)
-
+            self.set_pixel(index, r, g, b)
 
     def set_pixel(self, n, r, g, b):
         """Set a single pixel to RGB colour"""
-        ws2812.setPixelColorRGB(n, r, g, b)
+        self.pixels[n] = (r, g, b)
+        ws2812.setPixelColorRGB(n, GAMMA[r], GAMMA[g], GAMMA[b])
 
 
     def get_pixel(self, n):
         """Get the RGB value of a single pixel"""
         if n is not None:
-            pixel = ws2812.getPixelColor(n)
-            return (pixel>>16)&0xff, (pixel>>8)&0xff, pixel&0xff
+            return self.pixels[n]
 
 
     def set_pixels(self, pixels):
@@ -122,7 +140,7 @@ class Led_Controller:
             
     def get_pixels(self):
         """Get the RGB value of all pixels in a 1d array of 3d tuples"""
-        return [self.get_pixel(n) for n in range(0, self.total_leds)]
+        return self.pixels
 
     def show(self):
         show()
