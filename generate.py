6760251fd263404c799be59ec94724e2b551a7be
Nick Pesce <npesce@terpmail.umd.edu>
2017-07-01 03:23:44 +0000

    Updated api.ai json format

--- generate.py
+++ generate.py
@@ -13,19 +13,19 @@ if not os.path.exists('api-ai'):
 #API.AI colors
 colors = api_ai_list_format(np.get_colors())
 color_file = open('api-ai/colors.json', 'w')
-color_file.write(json.dumps(colors))
+color_file.write(json.dumps({'name': 'Colors', 'entries': colors}))
 
 #API.AI effects
 effects = api_ai_list_format(np.get_effects())
 effects_file = open('api-ai/effects.json', 'w')
-effects_file.write(json.dumps(effects))
+effects_file.write(json.dumps({'name': 'Effects', 'entries': effects}))
 
 #API.AI speeds
 speeds = api_ai_dict_format(np.get_speeds())
 speeds_file = open('api-ai/speeds.json', 'w')
-speeds_file.write(json.dumps(speeds))
+speeds_file.write(json.dumps({'name': 'Speeds', 'entries': speeds}))
 
 #API.AI sections
 sections = api_ai_dict_format(dict(np.get_sections(), **np.get_zones()))
 sections_file = open('api-ai/ranges.json', 'w')
-sections_file.write(json.dumps(sections))
+sections_file.write(json.dumps({'name': 'Ranges', 'entries': sections}))
