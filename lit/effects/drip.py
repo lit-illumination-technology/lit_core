3c95927408a701cc7d8f09213750292a200729ee
Nick Pesce <nickpesce22@gmail.com>
2019-07-07 00:07:08 -0400

    Tuned numbers

--- lit/effects/drip.py
+++ lit/effects/drip.py
@@ -6,7 +6,7 @@ start_string = name + " started!"
 description = "Mimics water droplets accumulating and falling"
 
 def setup_dullness(lights, args):
-    return [15]*lights.num_leds
+    return [0]*lights.num_leds
 
 schema = {
     'speed': {
@@ -40,7 +40,7 @@ def update(lights, step, state):
     color = state['color'];
     dullness = state['dullness']
     for i in range(lights.num_leds):
+        if dullness[i]<=1 or random.randint(0, int(dullness[i]*40)) == 0:
+            dullness[i] = random.random()*200
         lights.set_pixel(i, int(color[0]/dullness[i]), int(color[1]/dullness[i]), int(color[2]/dullness[i]))
         dullness[i] -= random.random()/20
-        if dullness[i]<=1 or random.randint(0, int(dullness[i]*20)) == 0:
-            dullness[i] = (random.random()*4)+4
