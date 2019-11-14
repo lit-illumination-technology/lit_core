307f72de3f86192024a162a3e0a3dfe8ae856911
Nick Pesce <nickpesce22@gmail.com>
2019-11-13 19:31:11 -0500

    Improve efficiency of Fade

--- lit/effects/fade.py
+++ lit/effects/fade.py
@@ -5,6 +5,10 @@ start_string = "Fading!"
 description = "Lights fade from one color to another"
 
 TOTAL_STEPS = 1000
+def calc_delta(lights, args):
+    ar, ag, ab = args['start color']
+    br, bg, bb = args['end color']
+    return ((br-ar)/TOTAL_STEPS, (bg-ag)/TOTAL_STEPS, (bb-ab)/TOTAL_STEPS)
 
 schema = {
     'speed': {
@@ -23,29 +27,36 @@ schema = {
             'default': (0, 0, 0)
         },
         'user_input': True,
-        'required': False
+        'required': False,
+        'index': 0
     },
     'end color': {
         'value': {
             'type': 'color',
-            'default': (0, 0, 0)
+            'default': (255, 255, 255)
         },
         'user_input': True,
-        'required': False
+        'required': False,
+        'index': 0
+    },
+    'delta': {
+        'value': {
+            'type': 'number tuple',
+            'default_gen': calc_delta
+        },
+        'user_input': False
     }
 }
 
 def update(lights, step, state):
-    start_color = state['start color']
-    end_color = state['end color']
     if step >= TOTAL_STEPS:
         #TODO hotswap effects
+        end_color = state['end color']
         lights.set_all_pixels(*end_color)
         return
-    ar, ag, ab = start_color
-    br, bg, bb = end_color
-    ds = ((br-ar)/TOTAL_STEPS, (bg-ag)/TOTAL_STEPS, (bb-ab)/TOTAL_STEPS)
-    nr = ar+ds[0]*step
-    ng = ag+ds[1]*step
-    nb = ab+ds[2]*step
-    lights.set_all_pixels(max(0,int(nr)), max(0,int(ng)), max(0,int(nb)))
+    ar, ag, ab = state['start color']
+    dr, dg, db = state['delta']
+    nr = ar+dr*step
+    ng = ag+dg*step
+    nb = ab+db*step
+    lights.set_all_pixels(max(0, int(nr)), max(0, int(ng)), max(0, int(nb)))
