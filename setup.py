9273957a24473c1dcae47f44268644193370330b
Nick Pesce <nickpesce22@gmail.com>
2020-09-02 22:32:31 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="lit-core",
-    version="0.0.0",
+    version="0.0.1",
     author="Nick Pesce",
     author_email="nickpesce22@gmail.com",
     description="Lit Ilumination Technology",
