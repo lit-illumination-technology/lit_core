2f8459a791265e01e0fe50ddc7cd37883288d471
Nick Pesce <nickpesce22@gmail.com>
2020-11-29 18:46:47 +0000

    Increment version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="lit-core",
-    version="0.0.1",
+    version="0.0.2",
     author="Nick Pesce",
     author_email="nickpesce22@gmail.com",
     description="Lit Ilumination Technology",
