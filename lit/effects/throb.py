823c55462c29011133dcc162d0022bd688e8d720
Nick Pesce <nickpesce22@gmail.com>
2021-06-16 03:24:36 +0100

    Improve throb colors

--- lit/effects/throb.py
+++ lit/effects/throb.py
@@ -11,18 +11,26 @@ schema = {
         "required": False,
     },
     "brightness": {"value": {"type": "number", "default": 0}, "user_input": False},
-    "delta": {"value": {"type": "number", "default": 0.05}, "user_input": False},
+    "delta": {"value": {"type": "number", "default": -0.05}, "user_input": False},
+    "lower_bound": {"value": {"type": "number", "default": 0.1}, "user_input": False},
+    "current_color": {"value": {"type": "color", "default": (0, 0, 0)}, "user_input": False},
 }
 
 
 def update(lights, step, state):
     brightness = state["brightness"]
-    color = state["color"].get_color(step)
+    color = state["current_color"]
+    lower_bound = state["lower_bound"]
+    if brightness + state["delta"] > 1:
+        state["delta"] *= -1
+    elif brightness + state["delta"] < lower_bound:
+        state["delta"] *= -1 
+        state["current_color"] = state["color"].get_color(step)
+
+    state["brightness"] += state["delta"]
+
     lights.set_all_pixels(
         int(color[0] * brightness),
         int(color[1] * brightness),
         int(color[2] * brightness),
     )
-    if brightness + state["delta"] > 1 or brightness + state["delta"] < 0:
-        state["delta"] *= -1
-    state["brightness"] += state["delta"]
