25806161547537ddf0ab3ce881b37d0ff2ba4e15
Nick Pesce <nickpesce22@gmail.com>
2021-11-07 23:45:42 +0000

    Update chase to use color generators

--- lit/effects/chase.py
+++ lit/effects/chase.py
@@ -12,7 +12,8 @@ description = (
 
 def create_heads(lights, args):
     num_heads = int(args["number"])
-    colors = [random.random() for _ in range(0, num_heads)]
+    print(args['color'])
+    colors = [args['color'].get_color(i) for i in range(0, num_heads)]
     head_locations = [i * (-lights.size // num_heads) for i in range(0, num_heads)]
     return [head_locations, colors]
 
@@ -31,15 +32,17 @@ schema = {
     },
     "color": {
         "value": {
-            "type": "color",  # TODO integer type (or step)
+            "type": "color",
             "default": {"type": "Random Colors"},
         },
         "user_input": True,
         "required": False,
+        "index": 0
     },
     "heads": {
         "value": {"type": "tuple list", "default_gen": create_heads},
         "user_input": False,
+        "index": 1
     },
 }
 
@@ -48,8 +51,8 @@ def update(lights, step, state):
     heads = state["heads"]
     for i in range(len(heads[0])):
         if heads[0][i] >= 0:
-            lights.set_pixel_hsv(heads[0][i], heads[1][i], 1, 1)
+            lights.set_pixel(heads[0][i], *heads[1][i])
         heads[0][i] += 1
         if heads[0][i] >= lights.size:
             heads[0][i] = 0
-            heads[1][i] = colorsys.rgb_to_hsv(state["color"].get_color(step))
+            heads[1][i] = state["color"].get_color(step)
