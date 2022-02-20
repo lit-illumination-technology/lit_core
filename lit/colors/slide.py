56b72e6717d6925d7523cb3602d31896a55374af
Nick Pesce <nickpesce22@gmail.com>
2022-02-20 17:51:54 +0000

    Support a frequency arg in the slide color generator

--- lit/colors/slide.py
+++ lit/colors/slide.py
@@ -3,13 +3,24 @@ import random
 
 name = "Slide"
 description = "Sliding spectrum of colors"
-schema = {}
+schema = {
+        "frequency": {
+            "value": {
+                "type": "number",
+                "min": 0,
+                "max": 5,
+                "default": 1
+                },
+            "user_input": True,
+            "required": False
+            }
+        }
 
 
 def get_generator(args):
     def generator(step, position):
         return tuple(
-            map(lambda x: int(255 * x), colorsys.hsv_to_rgb(((step + (position*100)) % 255) / 255, 1, 1))
+            map(lambda x: int(255 * x), colorsys.hsv_to_rgb(((step + (position*255*args["frequency"])) % 255) / 255, 1, 1))
         )
 
     return generator
